8dbb413b869afd9a69e43b946a45b597da36cfa2
Raul Gonzalez Moya <raul.gomo@gmail.com>
2014-10-24 23:51:27 +0200

    Arreglado el problema de los controladores de angular

--- public/coreCategorias.js
+++ public/coreCategorias.js
@@ -1,13 +1,13 @@
-// public/corePuntuaciones.js
-var scotchPuntuacion = angular.module('scotchPuntuacion', []);
+// public/coreCategorias.js
+var scotchCategoria = angular.module('scotchCategoria', []);
 
 function mainController($scope, $http) {
 	$scope.formData = {};
 
-	// when landing on the page, get all puntuaciones and show them
-	$http.get('/api/puntuaciones')
+	// when landing on the page, get all categorias and show them
+	$http.getCategorias('/api/cuentas/categorias')
 		.success(function(data) {
-			$scope.puntuaciones = data;
+			$scope.categorias = data;
 			console.log(data);
 		})
 		.error(function(data) {
@@ -15,11 +15,11 @@ function mainController($scope, $http) {
 		});
 
 	// when submitting the add form, send the text to the node API
-	$scope.createPuntuacion = function() {
-		$http.post('/api/puntuaciones', $scope.formData)
+	$scope.createCategoria = function() {
+		$http.post('/api/cuentas/categorias', $scope.formData)
 			.success(function(data) {
 				$scope.formData = {}; // clear the form so our user is ready to enter another
-				$scope.puntuaciones = data;
+				$scope.categorias = data;
 				console.log(data);
 			})
 			.error(function(data) {
@@ -27,11 +27,11 @@ function mainController($scope, $http) {
 			});
 	};
 
-	// delete a puntuacion after checking it
-	$scope.deletePuntuacion = function(id) {
-		$http.delete('/api/puntuaciones/' + id)
+	// delete a categoria after checking it
+	$scope.deleteCategoria = function(id) {
+		$http.delete('/api/cuentas/categoria/' + id)
 			.success(function(data) {
-				$scope.puntuaciones = data;
+				$scope.categorias = data;
 				console.log(data);
 			})
 			.error(function(data) {
